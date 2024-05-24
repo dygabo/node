be75ff2e6f8488ffaedb37a2741ec40586161a35
Joyee Cheung <joyeec9h3@gmail.com>
2024-05-24 15:36:24 +0200

    fixup! fixup! tmp

---
+++ test/es-module/test-module-hooks.js
@@ -0,0 +1,18 @@
+'use strict';
+
+require('../common');
+const fixtures = require('../common/fixtures');
+const { addHooks } = require('module');
+const { spawnSyncAndAssert } = require('../common/child_process');
+
+spawnSyncAndAssert(process.execPath,
+  [
+    '--require',
+    fixtures.path('transpiler-hook.js'),
+    fixtures.path('ts-log.ts')
+  ], {
+    trim: true,
+    stdout: 'VirtualPoint { x: 13, y: 56 }'
+  });
+
+// TODO(joyeecheung): require('xx.ts') here?
